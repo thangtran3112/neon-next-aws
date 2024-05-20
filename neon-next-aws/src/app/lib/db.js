2c14f303b7ccf6a82fb2e7547573c1b84b3567cf
Thang Tran <trathanl@amazon.com>
2024-05-20 12:41:13 -0700

    test insert a column from drizzle HTTP sql through Next Api Route

--- neon-next-aws/src/app/lib/db.js
+++ neon-next-aws/src/app/lib/db.js
@@ -1,12 +1,43 @@
-import { neon } from "@neondatabase/serverless";
-
+import { neon, neonConfig } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/postgres-js/driver";
+import { drizzle } from "drizzle-orm/neon-http";
 import { Config } from "sst/node/config";
 
-export async function dbClient() {
-  return neon(Config.DATABASE_URL);
+import * as schema from "@/app/lib/schema";
+
+/**
+ * Apart from migration, we will use Http client for CRUD operations instead of websocket
+ */
+export async function dbClient(useSQLOnly) {
+  neonConfig.fetchConnectionCache = true;
+  const sql = neon(Config.DATABASE_URL);
+  if (useSQLOnly) {
+    //neon SQL client
+    return sql;
+  }
+  // drizle ORM client
+  return drizzle(sql);
 }
 
 export async function dbNow() {
-  const sql = await dbClient();
+  const sql = await dbClient(true);
   return sql`SELECT NOW()`;
+}
+
+/**
+ * addLead will isert a row into database through drizzle client
+ * We can also use neon client directly through dbClient(true)
+ */
+export async function addLead({ email }) {
+  const db = await dbClient(false);
+
+  //https://orm.drizzle.team/docs/insert
+  const dbResult = await db
+    .insert(schema.LeadTable)
+    .values({ email: email })
+    .returning({ timestamp: schema.LeadTable.createdAt });
+  if (dbResult.length === 1) {
+    return dbResult[0].timestamp;
+  }
+  return;
 }
